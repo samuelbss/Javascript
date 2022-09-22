cfad02a35be073ecb7be2fbd99947be9eb65a2d7
Samuel André <109756191+samuelbss@users.noreply.github.com>
2022-09-22 12:06:43 -0300

    Condições Compostas e Múltiplas!

---
+++ aula12/ex011.js
@@ -0,0 +1,8 @@
+var idade = 12
+if (idade < 16) {
+    console.log(' Você não vota!')
+} else if (idade <18 || idade > 67){
+    console.log ('Voto opcional')
+} else {
+    console.log ('Voto obrigatório')
+}
